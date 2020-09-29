d4f9900e74158f6c3b5210f2b1c4fd58a2dbec7e
luiz.darbem <luiz.darbem@tempoassist.com.br>
2020-09-29 10:44:30 -0300

    Adicinado tempo de expiração do cache

--- desafio/src/main/java/br/com/luizalabs/desafio/configuration/OAuth2ServerConfiguration.java
+++ desafio/src/main/java/br/com/luizalabs/desafio/configuration/OAuth2ServerConfiguration.java
@@ -5,7 +5,6 @@ import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Primary;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
@@ -18,7 +17,6 @@ import org.springframework.security.oauth2.config.annotation.web.configuration.E
 import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
 import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerEndpointsConfigurer;
 import org.springframework.security.oauth2.config.annotation.web.configurers.ResourceServerSecurityConfigurer;
-import org.springframework.security.oauth2.provider.token.DefaultTokenServices;
 import org.springframework.security.oauth2.provider.token.TokenStore;
 import org.springframework.security.oauth2.provider.token.store.redis.RedisTokenStore;
 
@@ -115,16 +113,6 @@ public class OAuth2ServerConfiguration {
             ;
 
         }
-
-//        @Bean
-//        @Primary
-//        public DefaultTokenServices tokenServices() {
-//            DefaultTokenServices tokenServices = new DefaultTokenServices();
-//            tokenServices.setSupportRefreshToken(true);
-//            tokenServices.setTokenStore(redisTokenStore());
-//            tokenServices.set
-//            return tokenServices;
-//        }
 
     }
 
